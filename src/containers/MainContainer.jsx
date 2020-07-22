bcde87524bdae6efe0641df78baeaedbc8b3d6fb
JoLeaper <joleap123@gmail.com>
2020-07-22 13:46:16 -0700

    state defined, passed to components

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -4,12 +4,36 @@ import { RequestContent } from '../components/RequestContent/RequestContent'
 import { ResponseWindow } from '../components/ResponseWindow/ResponseWindow'
 
 export default class MainContainer extends Component {
+  state = {
+    apiLink: '',
+    requestMethod: 'GET',
+    requestBody: {},
+    response: {}
+  }
+
+  handleChange = (e) => {
+    const newState = {};
+    newState[e.target.name] = e.target.value;
+    this.setState(newState);
+  }
+
   render() {
+    const { apiLink, requestMethod, requestBody, response } = this.state;
     return (
       <div>
-        <LinkBar />
-        <RequestContent />
-        <ResponseWindow />
+        <LinkBar 
+        handleChange={this.handleChange} 
+        apiLink={apiLink}
+        />
+        <RequestContent 
+        handleChange={this.handleChange} 
+        requestBody={requestBody} 
+        requestMethod={requestMethod}
+        />
+        <ResponseWindow 
+        handleChange={this.handleChange} 
+        response={response} 
+        />
       </div>
     )
   }
